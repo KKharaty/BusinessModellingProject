b60c6d0b7f16152504164f15ea9f4e96beea20f1
Frankjr109 <61740841+Frankjr109@users.noreply.github.com>
2021-12-07 10:37:05 +0000

    Update DisplayItemGUI.java

--- BusinessModellingProject/src/termproject/DisplayItemGUI.java
+++ BusinessModellingProject/src/termproject/DisplayItemGUI.java
@@ -1,5 +1,75 @@
 package termproject;
+import java.util.ArrayList;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 
 public class DisplayItemGUI {
+  
+  JPanel pnl;
+	JLabel lblItems;
+	JComboBox comboBoxItems;
+	JButton btnHome;
+	
+	ArrayList<Item> items;
+	
+	public DisplayItemGUI(ArrayList<Item> itemlist)
+	{	items = itemlist;
+		createGUI();
+	}
+	
+	public String[] gatherItemDetails()
+	{
+		String[] itemNames = new String [items.size()];
+		
+			for(int i = 0; i < items.size(); i++)
+			{
+				String allItem = "";
+				Item item = items.get(i);
+				String itemname = item.toString();
+				itemNames[i] = itemname;
+			}
+		return itemNames;
+	}
+	
+	 public void createGUI() {
+
+	    	pnl = new JPanel();
+	    	lblItems = new JLabel("List of Items in Stock");
+	    	//comboBoxCustomer = new JComboBox(gatherItemDetails());
+	    	comboBoxItems = new JComboBox(items.toArray());
+	    	btnHome = new JButton("Return"); //Return button that will bring the user back to the home screen.
+	    	pnl.add(lblItems);
+	    	pnl.add(comboBoxItems);
+	    	pnl.add(btnHome);
+			add(pnl, BorderLayout.CENTER);
+			
+			btnHome.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+			
+			
+		}
+	 
+	 public static void main(String[] args) {
+			DisplayItemGUI displayGUI = new DisplayItemGUI(new ArrayList());
+			displayGUI.setTitle("Display Items in Stock");
+			displayGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			displayGUI.setSize(300, 300);
+			displayGUI.setVisible(true);
+
+		}
+	
 
 }
